5bcbf9ffcbf66b2c9507406606b4d8aa7317e152
rafaelgsousa <elderrafaelgomes@gmail.com>
2023-05-12 10:33:43 -0300

    refactor routes in routes/notes

--- routes/notes.js
+++ routes/notes.js
@@ -1,48 +1,46 @@
 import { Router } from 'express';
+
 import service from '../service/index.js';
 
 const router = Router();
 
-router.get('', async(_req, res) => {
+router.get('', async (_req, res) => {
     const notes = await service.getNotes();
-    res.render('notes/home', { notes() { return notes } });
+    res.render('notes/home', { notes });
 });
 
 router.get('/create', (_req, res) => {
     res.render('notes/create');
 });
 
-router.get('/:id', async(req, res) => {
+router.get('/:id', async (req, res) => {
     const { id } = req.params;
     const note = await service.getNoteById(id);
     res.render('notes/detail', { note });
 });
 
-router.get('/edit/:id', async(req, res) => {
+router.get('/edit/:id', async (req, res) => {
     const { id } = req.params;
     const note = await service.getNoteById(id);
     res.render('notes/edit', { note });
 });
 
-
-router.post('', (req, res) => {
-
+router.post('', async (req, res) => {
     const { title, description } = req.body;
-    service.insertNode({ title, description });
-
-    res.redirect(301,'/');
+    await service.insertNode({ title, description });
+    res.redirect(301, '/');
 });
 
-router.post('/update', async(req, res) => {
+router.post('/update', async (req, res) => {
     const { id, title, description } = req.body;
     await service.updateNoteById(id, { title, description });
-    res.redirect(301,'/');
+    res.redirect(301, '/');
 });
-
-router.post('/delete', async(req, res) => {
+  
+router.post('/delete', async (req, res) => {
     const { id } = req.body;
     await service.deleteNote(id);
-    res.redirect(301,'/');
+    res.redirect(301, '/');
 });
 
 export default router;
